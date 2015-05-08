b7b796220719a9a5991bfaf72b9bda9aae4724ba
lag1101 <lomanovasiliy@gmail.com>
2015-05-08 13:18:41 +0400

    added product list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var Products = require('../Products');
+
+var products = new Products();
+
+products.load(function(err, list){
+    if(err)
+        console.error(err);
+    else
+        console.log(list);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/list', function(req, res, next) {
+    res.send(products.list);
+});
+router.post('/newProduct', function(req, res, next) {
+    var newProduct = req.body;
 
+    products.push(newProduct);
+    products.save();
+    res.end();
+});
 module.exports = router;
