380f9b633540a3d4d13d46c05b216e248f38c0c4
lag1101 <lomanovasiliy@gmail.com>
2015-05-13 11:59:00 +0400

    routes moved into module

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var products = require('./routes/products');
 
 var app = express();
 
@@ -24,9 +21,7 @@ app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/products', products);
+require('./routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var products = require('../products').products;
-products.load(function(err, list){
-    if(err)
-        console.error(err);
-    else
-        console.log(list);
-});
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.get('/list', function(req, res, next) {
-    res.send(products.list);
-});
-router.post('/newProduct', function(req, res, next) {
-    var newProduct = req.body;
-
-    products.push(newProduct);
-    products.save();
-    res.end();
-});
-router.post('/removeProduct', function(req, res, next) {
-    var product = req.body;
-
-    products.list.map(function(obj, index){
-        if(obj.description === product.description)
-            products.list.splice(index, 1);
-    });
-    products.save();
-    res.end();
-});
-router.get('/daily', function(req, res, next) {
-    var date = req.query.date;
-    res.send(products.getDaily(date));
-});
-router.post('/daily', function(req, res, next) {
-    var dailyProduct = req.body;
-
-    products.addDaily(dailyProduct.date, dailyProduct.products);
-    products.save();
-    res.end();
-});
+/**
+ * Created by vasiliy.lomanov on 13.05.2015.
+ */
 
 
-module.exports = router;
+module.expotrs = function(app){
+    app.use('/', require('./main'));
+    app.use('/users', require('./users'));
+    app.use('/products', require('./products'));
+};
