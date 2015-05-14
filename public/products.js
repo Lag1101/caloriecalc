ef07d2702170969507254fdccecc703b846524c8
Lag1101 <lomanovvasiliy@gmail.com>
2015-05-14 23:01:20 +0300

    fix sorting

--- public/products.js
+++ public/products.js
@@ -143,9 +143,9 @@
 
         function comp(p1, p2){
             if(sortKey==='description' || sortKey==='details')
-                return (order === "greater") ^ (p1[sortKey] < p2[sortKey]);
+                return (p1[sortKey] < p2[sortKey]);
             else
-                return (order === "greater") ^ (parseFloat(p1[sortKey]) < parseFloat(p2[sortKey]));
+                return (parseFloat(p1[sortKey]) < parseFloat(p2[sortKey]));
         }
         reorderProducts = reorderProducts.sort(comp);
 
