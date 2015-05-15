a1d098b1369f0efdfeb94b5d8feeb23ef1c1eaeb
Lag1101 <lomanovvasiliy@gmail.com>
2015-05-15 23:39:24 +0300

    test socket.broadcast

--- socket/index.js
+++ socket/index.js
@@ -20,7 +20,7 @@ module.exports = function(server){
                 console.info('disconnected');
             })
             .on('list', function(){
-                socket.emit('list', products.list);
+                socket.broadcast.emit('list', products.list);
             })
             .on('newProduct', function(newProduct){
                 products.push(newProduct);
@@ -31,21 +31,21 @@ module.exports = function(server){
                 products.save();
             })
             .on('getDaily', function(date){
-                socket.emit('getDaily', products.getDaily(date));
+                socket.broadcast.emit('getDaily', products.getDaily(date));
             })
             .on('setDaily', function(dailyProduct){
                 products.addDaily(dailyProduct.date, dailyProduct.products);
                 products.save();
             })
             .on('getCurrentDishProducts', function(){
-                socket.emit('getCurrentDishProducts', products.currentDish);
+                socket.broadcast.emit('getCurrentDishProducts', products.currentDish);
             })
             .on('setCurrentDishProducts', function(currentDish){
                 products.currentDish = currentDish;
                 products.save();
             })
             .on('getCurrentDate', function(){
-                socket.emit('getCurrentDate', products.currentDish.date);
+                socket.broadcast.emit('getCurrentDate', products.currentDish.date);
             })
             .on('setCurrentDate', function(date){
                 products.currentDish.date = date;
