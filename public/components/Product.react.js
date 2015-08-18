5e4759679cb353a4b606c3e8e870b888a480988b
lag1101 <lomanovasiliy@gmail.com>
2015-08-18 20:09:15 +0400

    addded [PureRenderMixin

--- public/components/Product.react.js
+++ public/components/Product.react.js
@@ -7,8 +7,10 @@ var socket = require('../socket');
 var React = require('react');
 var TextInput = require('./Input.react.js').TextInput;
 var NumericInput = require('./Input.react.js').NumericInput;
+var PureRenderMixin = require('react/addons').addons.PureRenderMixin;
 
 var Product = React.createClass({
+    mixins: [PureRenderMixin],
     getProduct: function(){
         var product = this.props.product;
         return {
